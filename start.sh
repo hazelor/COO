cefe65719d6e8f4ad116df7fc0b30af9c59045b1
guo xiao <viva_6059@163.com>
2016-02-26 01:58:54 +0000

    test

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo echo 3 > /sys/devices/virtual/misc/gpio/mode/gpio0
-sudo echo 3 > /sys/devices/virtual/misc/gpio/mode/gpio1
-sudo echo 3 > /sys/devices/virtual/misc/gpio/mode/gpio2
-
-
-cd /home/linaro/COO
-
-python main.py
