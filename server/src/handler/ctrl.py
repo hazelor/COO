241d39208fa5465c07f52f1921336d389ba948c5
guo xiao <viva_6059@163.com>
2016-05-29 10:44:57 +0800

    add new data

--- server/src/handler/ctrl.py
+++ server/src/handler/ctrl.py
@@ -3,24 +3,47 @@ __author__ = 'sonic-server'
 from base import base_handler
 import tornado.web
 import tornado.ioloop
-# import time
+import time
 import tcelery
-import tasks
+import tasks_for_syn
 import json
+import tornado.gen
+import gc
 
-
-tcelery.setup_nonblocking_producer()
+# tcelery.setup_nonblocking_producer()
 
 class ctrl_handler(base_handler):
     @tornado.web.asynchronous
+    @tornado.gen.coroutine
     def post(self):
         jdata = json.loads(self.request.body)
+        print "ctrl:",jdata
         mac_address = jdata['mac_address']
         sql="select duration from device WHERE mac_address='%s' "%(mac_address)
-        tasks.db_query_basic.apply_async(args=[sql], callback=self.on_success)
-    def on_success(self, resp):
-        resp = resp.result[0][0]
-        # resp = json.dumps()
-        # print resp
-        self.write(resp)
+        # response = yield tornado.gen.Task(tasks.db_query_basic.apply_async, args=[sql])
+        # response = yield tornado.gen.Task(tasks.db_query_basic, args=[sql])
+        response = yield tasks_for_syn.db_query_basic(sql)
+        # if response.result:
+        #     data = response.result[0][0]
+        #     self.write(data)
+        if response:
+            data = response[0][0]
+            data = str(int(data)-1)
+            self.write(data)
         self.finish()
+        # tornado.ioloop.IOLoop.instance().add_callback(self.finish)
+    # @tornado.web.asynchronous
+    # @tornado.gen.coroutine
+    def on_success(self, resp):
+        # if resp == '':
+        #     self.write(resp)
+        # yield tornado.gen.Task(tornado.ioloop.IOLoop.instance().add_timeout,time.time()+200)
+        if resp.result:
+            data = resp.result[0][0]
+            self.write(data)
+        # tornado.ioloop.IOLoop.instance().add_callback(self.finish())
+        self.finish()
+    # def on_finish(self):
+        # print 'on_finish method called'
+    def on_connection_close(self):
+        print 'ctrl on_connection_close method called'
