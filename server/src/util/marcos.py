4b76f0f392c21307e683804301e8f3861e27b792
guo xiao <viva_6059@163.com>
2016-05-29 10:45:29 +0800

    add new data

--- server/src/util/marcos.py
+++ server/src/util/marcos.py
@@ -1,11 +1,16 @@
 #about db
-DB_USER = 'root'
-# DB_USER = 'sonic513'
+# DB_USER = 'root'
+DB_USER = 'sonic513'
 
 DB_USER_PASSWORD = 'sonic513'
 
 DB_NAME = 'carbon_dioxide'
 
+# DB_HOST = 'rds610d34eqqqelm02gz.mysql.rds.aliyuncs.com'
+
+DB_HOST = 'rds6mt3h5p99gt63v297.mysql.rds.aliyuncs.com'
+
+DB_HOST_PORT = 3306
 ##-----chamber conf------
 
 CHAMBER_CONF_PATH = "conf/chambers.conf"
